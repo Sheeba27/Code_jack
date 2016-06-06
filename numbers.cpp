62dfc58c209d18f9657c99467e7d9aa59d0073f3
Sheeba27 <sheebabtechit@gmail.com>
2016-06-06 11:23:15 +0530

    Create numbers.cpp

---
+++ numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	cin>>number;
+	if(number==0)
+		cout<<"zero";
+	else
+		(number>0)?cout<<"positve":cout<<"negative";
+		return 0;
+}	
